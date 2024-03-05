b17792c6da8db308fca87deb19213afce6ef7b61
hitsutakarin77@gmail.com <hitsutakarin77@gmail.com>
2024-03-05 07:19:22 +0700

    aaa kasian a

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["i.ibb.co"],
+  },
+  output: "export",
+};
 
 export default nextConfig;
