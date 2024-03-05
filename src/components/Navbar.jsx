7f62b97d6ce32512da93f1b69258732f7523c41e
hitsutakarin77@gmail.com <hitsutakarin77@gmail.com>
2024-03-05 13:03:15 +0700

    feat: fixing bug sign out

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -131,6 +131,7 @@ export default function Navbar() {
             </li>
             <button
               type="button"
+              onClick={() => handleLogout()}
               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Sign Out
