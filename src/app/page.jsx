6fe288f08d754bf4b8ff724478ce79a6634f2104
hitsutakarin77@gmail.com <hitsutakarin77@gmail.com>
2024-03-08 16:16:55 +0700

    feat: responsive login page2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,6 +16,10 @@ export default function Login() {
 
   const router = useRouter();
 
+  if (Cookies.get("loggedmacaddress")) {
+    router.push("/home");
+  }
+
   const handleLogin = async (e) => {
     e.preventDefault();
     if (
@@ -96,7 +100,7 @@ export default function Login() {
           {alert ? (
             <div
               id="toast-danger"
-              class="fixed flex items-center w-full max-w-sm md:max-w-lg p-4 space-x-4 text-gray-500 bg-white divide-x rtl:divide-x-reverse divide-gray-200 rounded-lg shadow right-5 bottom-5 dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800"
+              class="fixed flex items-center w-4/5 md:w-full md:max-w-lg p-4 space-x-4 text-gray-500 bg-white divide-x rtl:divide-x-reverse divide-gray-200 rounded-lg shadow right-5 bottom-5 dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800"
               role="alert"
             >
               <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
